f20ee805c0e2e85849b0aabe49d5e1a43fd1052f
c-andres <charlesalexanderandres@gmail.com>
2017-09-21 17:26:40 -0700

    final commit before class

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,47 +1,3 @@
-// <script>
-
-
-
-//   var track = [];
-
-//     var maxQues = prompt("How many questions do you want to answer?", "5");
-//     var rand = Math.floor(Math.random() * maxQues);
-
-//     var str='<h4>Answer all the questions</h4>';
-
-//     for(var i=0;i<maxQues;i++){
-
-//         var rand = Math.floor(Math.random() * maxQues);
-//         str+=(i+1)+'.  '+quizObj[rand].question+'<br>';
-//         str+='<form><table>'+
-//             '<tr><td id="a1"><input type="radio" name="radio' + i + '"/>'+'&nbsp;&nbsp;'+quizObj[rand].choice[0]+'</td></tr>'+
-//             '<tr><td id="a2"><input type="radio" name="radio' + i + '" />'+'&nbsp;&nbsp;'+quizObj[rand].choice[1]+'</td></tr>'+
-//             '<tr><td id="a3"><input type="radio" name="radio' + i + '"/>'+'&nbsp;&nbsp;'+quizObj[rand].choice[2]+'</td></tr>'+
-//         '</table></form><br>';
-//     track[i]=rand;
-//     }
-//     str += '<input value="Submit" type="button" onclick="scoreMe()"><br><br>';
-//     str += 'Score: <input id="score" type="text" size="8" ><br><br><br>';
-//     document.getElementById('quesRes').innerHTML = str;
-
-//   function scoreMe(){
-//     var sum=0;
-//     for(var j=0;j<maxQues;j++){
-//       for(var k=0;k<3;k++){
-//         if(quizObj[track[j]].choice[k].checked===quizObj[track[j]].correct[0]){
-//           console.log('Works'+j);
-//           sum++;
-//         }
-//       }
-//     }
-//     document.getElementById('score').value = ((sum/maxQues)*100)+'%';
-//   }
-// </script>
-
-
-//shorthand for document.ready function
-// Declaration and Data Type & Valuse assignment of Variables
-
 var questionNumber = 1;
 var bolQuestionPage = false;	
 var testResult = "";
@@ -50,39 +6,6 @@ var randomQuestionIndex = 0;
 var numberCorrect = 0;
 var numberIncorrect = 0;
 var numberUnanswered = 0;
-var arrPhilosopherChoicess = [
-	"Socrates",
-	"Plato",
-	"Epicurus",
-	"Aristotle",
-	"Marcus Aurelius",
-	// "Augustine",
-	"Thomas Aquinas",
-	"Michel de Montaigne",
-	"Blaise Pascal",
-	"La Rouchfoucauld",
-	"Baruch Spinoza",
-	"Rene Descartes",
-	"David Hume",
-	"Immanuel Kant",
-	"Edmund Burke",
-	"Hegel",
-	"Schopenhauer",
-	"Soren Kierkegaard",
-	"Friedrich Nietzsche",
-	"Ludwig Wittgenstein",
-	"Heidegger",
-	"Jean Paul Sartre",
-	"Albert Camus",
-	"Jaques Derrida",
-	"Michael Foucault",
-	"Emil Cioran"
-];
-console.log(arrPhilosopherChoicess.length);
-
-var objectQuestions = {
-	arrQuestion1: ['Who famously coined the phrase " I think therefore I am"?', "Renes Descartes"]
-}
 var arrCorrectAnswer = ["Correct!", "You Got It!", "Right-o!", "You're Doing it Peter!", "Correctamundo!", "You're Right!"]
 var arrIncorrectAnswer =["Wrong!", "Nope!", "Study Up!", "Better Luck Next Time", "That's a Negative GhostRider", "Nuh-uh!"]
 var objTrivia = [
@@ -140,7 +63,7 @@ var objTrivia = [
       "answers": ["Plato","Epicurus","Socrates","Aristotle"],  
       "correct": "Aristotle",
       "src": "assets/images/aristotle.png",
-      "quote": ""
+      "quote": "We are what we repeatedly do. Excellence, then, is not an act, but a habit."
     },
     { 
       "question": "Which philosopher introduced the idea of the Sublime?",
@@ -189,7 +112,7 @@ var objTrivia = [
       "answers": ["Georg Hegel", "Friedrich Nietzsche", "Plato", "Martin Heidegger"],  
       "correct": "Georg Hegel",
       "src": "assets/images/hegel.png",
-      "quote": ""
+      "quote": "Genuine tragedies in the world are not conflicts between right and wrong. They are conflicts between two rights."
     },
     { 
       "question": 'Whose philosophy revolved around the confrontation of "Das Nichts" or "The Nothing"?',
@@ -224,7 +147,7 @@ var objTrivia = [
       "answers": ["Soren Kierkegaard", "Georg Hegel", "Arthur Schoppenhauer", "Friedrich Nietzsche"],  
       "correct": "Arthur Schoppenhauer",
       "src": "assets/images/schoppenhauer.png",
-      "quote": ""
+      "quote": "Change alone is eternal, perpetual, immortal."
     },
     { 
       "question": "Which philosopher despised democracy and the foolish means in which it steers society?",
@@ -265,15 +188,10 @@ $(function() {
 		textGenerator()
 		timer();	
 	});
-
-
-
-
 	//function that stores the index of the clicked answer button in a var
 	$(".answer-choice").click(function() {
       clickedId = $(this).attr("id");
   		clickedIdIndex = parseInt(clickedId.substring(clickedId.length - 1) - 1);
-  		console.log(clickedIdIndex);
   		answerChecker()
   		bolQuestionPage = true;
   		questionNumberDisplay();
@@ -292,192 +210,159 @@ $(function() {
 		clickedId = "";
 		randomQuestionIndex = 0;
 	});
-
-//function that generates a random index to generate a new question
-function questionRandomizer(){
-	randomQuestionIndex = Math.floor(Math.random() * objTrivia.length);
-	console.log(randomQuestionIndex);
-
-};
-//function that splices the indexed random number from the objTrivia array
-function questionSplicer(){
-		objTrivia.splice([randomQuestionIndex], 1);
-};
-
-//function that takes title, question, answer || results as argument and hides respective page
-function hidePage(pageName){
-	$("#" + pageName + "-page").addClass("hide");
-};
-//function that takes title, question, answer || results as argument and displays respective page
-function showPage(pageName){
-	$("#" + pageName + "-page").removeClass("hide");
-};
-//function that updates the question number
-function questionNumberDisplay(){
-	$("#question-number").text("Question # " + questionNumber);
-	questionNumber++;
-}
-
-
-//function that generates text for 
-function textGenerator(){
-	questionRandomizer();
-	$("#question-text").text(objTrivia[randomQuestionIndex].question);
-	for (var i = 1; i <= objTrivia[randomQuestionIndex].answers.length; i++) {
-		$("#answer-" + i).text(objTrivia[randomQuestionIndex].answers[i-1]);
+	//function that generates a random index to generate a new question
+	function questionRandomizer(){
+		randomQuestionIndex = Math.floor(Math.random() * objTrivia.length);
 	};
-	timer();
-	;
-};
-console.log(randomQuestionIndex);
-//function that generates updated information for question page and transitions to next question page
-function nextQuestion(){
-	textGenerator();
-	showPage("question");
-	hidePage("answer");
-}
-function answerChecker(){
-	console.log("ClickedIdIndex is " + clickedIdIndex);
-	console.log("randomQuestionIndex is " + randomQuestionIndex);
-	if (questionNumber > 10)
-		resultsPage();
+	//function that splices the indexed random number from the objTrivia array
+	function questionSplicer(){
+			objTrivia.splice([randomQuestionIndex], 1);
+	};
 
-	else if (objTrivia[randomQuestionIndex].answers[clickedIdIndex] === objTrivia[randomQuestionIndex].correct){
-		numberCorrect++;
-		correctAnswerPage();
-
+	//function that takes title, question, answer || results as argument and hides respective page
+	function hidePage(pageName){
+		$("#" + pageName + "-page").addClass("hide");
+	};
+	//function that takes title, question, answer || results as argument and displays respective page
+	function showPage(pageName){
+		$("#" + pageName + "-page").removeClass("hide");
+	};
+	//function that updates the question number
+	function questionNumberDisplay(){
+		$("#question-number").text("Question # " + questionNumber);
+		questionNumber++;
 	}
-	else if (objTrivia[randomQuestionIndex].answers[clickedIdIndex] !== objTrivia[randomQuestionIndex].correct){
-		numberIncorrect++;
-		incorrectAnswerPage();
-	}
-	else
-		return -1
-}
-
-function correctAnswerPage(){
-	hidePage("question");	
-	showPage("answer");
-	//line of code that displays random correct answer affirmation
-	$("#answer-result").text(arrCorrectAnswer[Math.floor(Math.random()*arrCorrectAnswer.length)]);
-	$("#answer-image").attr("src", objTrivia[randomQuestionIndex].src);
-	$("#answer-text").text('"'+ objTrivia[randomQuestionIndex].quote +'"');
-	transitionTimer();
-	questionSplicer();
-};
-
-function timesUpPage(){
-	hidePage("question");	
-	showPage("answer");
-	//line of code that displays random correct answer affirmation
-	$("#answer-result").text("Times Up!");
-	$("#answer-image").attr("src", objTrivia[randomQuestionIndex].src);
-	$("#answer-text").text("Correct Answer: " + objTrivia[randomQuestionIndex].correct);
-	numberUnanswered ++
-	transitionTimer();
-	questionSplicer();
-};
-function incorrectAnswerPage(){
-	hidePage("question");	
-	showPage("answer");
-	//line of code that displays random incorrect answer negation
-	$("#answer-result").text(arrIncorrectAnswer[Math.floor(Math.random() * arrIncorrectAnswer.length)]);
-	$("#answer-image").attr("src", objTrivia[randomQuestionIndex].src);
-	$("#answer-text").text("Correct Answer: " + objTrivia[randomQuestionIndex].correct);
-	transitionTimer();
-	questionSplicer();
-};
-
-function resultsPage(){
-	hidePage("answer");
-	hidePage("question")
-	showPage("results");
-	$("#correct-answers").text("Correct Answers: " + numberCorrect);
-	$("#incorrect-answers").text("Incorrect Answers: " + numberIncorrect);
-	$("#unanswered-questions").text("Unanswered Questions: " + numberUnanswered);	
-}
-
-// console.log(testResult)
 
 
+	//function that generates text for 
+	function textGenerator(){
+		questionRandomizer();
+		$("#question-text").text(objTrivia[randomQuestionIndex].question);
+		for (var i = 1; i <= objTrivia[randomQuestionIndex].answers.length; i++) {
+			$("#answer-" + i).text(objTrivia[randomQuestionIndex].answers[i-1]);
+		};
+		timer();
+		;
+	};
+	//function that generates updated information for question page and transitions to next question page
+	function nextQuestion(){
+		textGenerator();
+		showPage("question");
+		hidePage("answer");
+	};
+	function answerChecker(){
+		if (questionNumber > 10)
+			resultsPage();
 
+		else if (objTrivia[randomQuestionIndex].answers[clickedIdIndex] === objTrivia[randomQuestionIndex].correct){
+			numberCorrect++;
+			correctAnswerPage();
 
+		}
+		else if (objTrivia[randomQuestionIndex].answers[clickedIdIndex] !== objTrivia[randomQuestionIndex].correct){
+			numberIncorrect++;
+			incorrectAnswerPage();
+		}
+		else
+			return -1
+	};
 
-function transitionTimer(){
-	var transitionTimerCount = 6;
-	var intervalId2 = "";
-	function run() {
-    intervalId2 = setInterval(decrement, 1000);
-  }
-	function decrement() {
-    //  Decrease timerCount by one.
-    transitionTimerCount--;
-    
-    if (transitionTimerCount === 0) {
-    	stop();
-    	nextQuestion();
-    	bolQuestionPage = false;
+	function correctAnswerPage(){
+		hidePage("question");	
+		showPage("answer");
+		//line of code that displays random correct answer affirmation
+		$("#answer-result").text(arrCorrectAnswer[Math.floor(Math.random()*arrCorrectAnswer.length)]);
+		$("#answer-image").attr("src", objTrivia[randomQuestionIndex].src);
+		$("#answer-text").text('"'+ objTrivia[randomQuestionIndex].quote +'"');
+		transitionTimer();
+		questionSplicer();
+	};
 
-
-
-    }
-   	function stop() {
-
-    //  Clears our intervalId
-    //  We just pass the name of the interval
-    //  to the clearInterval function.
-    clearInterval(intervalId2);
-  	};
-	}
-	run();
-
-};
- 
-
-
-
-function timer() {
-  //  The run function sets an interval
-  //  that runs the decrement function once a second.
-  var timerCount = 20;
-  $("#show-timerCount").text("Time Remaining: " + timerCount + " seconds");
-  var intervalId = "";
-
-  function run() {
-    intervalId = setInterval(decrement, 1000);
-  }
-
-  //  The decrement function.
-  function decrement() {
-    //  Decrease timerCount by one.
-    timerCount--;
-    //  Show the timerCount in the #show-timerCount tag.
-    $("#show-timerCount").text("Time Remaining: " + timerCount + " seconds");
-    //  Once timerCount hits zero...
-    
-    if (bolQuestionPage === true){ 
-	    stop();
-	   }
-
-    else if	(timerCount === 0) {
-    	stop();
-    	timesUpPage();
-    }
-
-  }
-  function stop() {
-
-    //  Clears our intervalId
-    //  We just pass the name of the interval
-    //  to the clearInterval function.
-    clearInterval(intervalId);
-  }
-
-  //  Execute the run function.
-  run();
-
-
-//  Execute the run function.
-};
-
+	function timesUpPage(){
+		hidePage("question");	
+		showPage("answer");
+		//line of code that displays random correct answer affirmation
+		$("#answer-result").text("Times Up!");
+		$("#answer-image").attr("src", objTrivia[randomQuestionIndex].src);
+		$("#answer-text").text("Correct Answer: " + objTrivia[randomQuestionIndex].correct);
+		numberUnanswered ++
+		transitionTimer();
+		questionSplicer();
+	};
+	function incorrectAnswerPage(){
+		hidePage("question");	
+		showPage("answer");
+		//line of code that displays random incorrect answer negation
+		$("#answer-result").text(arrIncorrectAnswer[Math.floor(Math.random() * arrIncorrectAnswer.length)]);
+		$("#answer-image").attr("src", objTrivia[randomQuestionIndex].src);
+		$("#answer-text").text("Correct Answer: " + objTrivia[randomQuestionIndex].correct);
+		transitionTimer();
+		questionSplicer();
+	};
+	function resultsPage(){
+		hidePage("answer");
+		hidePage("question")
+		showPage("results");
+		$("#correct-answers").text("Correct Answers: " + numberCorrect);
+		$("#incorrect-answers").text("Incorrect Answers: " + numberIncorrect);
+		$("#unanswered-questions").text("Unanswered Questions: " + numberUnanswered);	
+	};
+	function transitionTimer(){
+		var transitionTimerCount = 6;
+		var intervalId2 = "";
+		function run() {
+	    intervalId2 = setInterval(decrement, 1000);
+	  }
+		function decrement() {
+	    //  Decrease timerCount by one.
+	    transitionTimerCount--;
+	    
+	    if (transitionTimerCount === 0) {
+	    	stop();
+	    	nextQuestion();
+	    	bolQuestionPage = false;
+	    }
+	   	function stop() {
+	    //  Clears our intervalId2
+	    //  We just pass the name of the interval
+	    //  to the clearInterval function.
+	    clearInterval(intervalId2);
+	  	};
+		}
+		run();
+	};
+	function timer() {
+	  //  The run function sets an interval
+	  //  that runs the decrement function once a second.
+	  var timerCount = 20;
+	  $("#show-timerCount").text("Time Remaining: " + timerCount + " seconds");
+	  var intervalId = "";
+	  function run() {
+	    intervalId = setInterval(decrement, 1000);
+	  }
+	  //  The decrement function.
+	  function decrement() {
+	    //  Decrease timerCount by one.
+	    timerCount--;
+	    //  Show the timerCount in the #show-timerCount tag.
+	    $("#show-timerCount").text("Time Remaining: " + timerCount + " seconds");
+	    //  Once timerCount hits zero...
+	    if (bolQuestionPage === true){ 
+		    stop();
+		   }
+	    else if	(timerCount === 0) {
+	    	stop();
+	    	timesUpPage();
+	    }
+	  }
+	  function stop() {
+	    //  Clears our intervalId
+	    //  We just pass the name of the interval
+	    //  to the clearInterval function.
+	    clearInterval(intervalId);
+	  }
+	  //  Execute the run function.
+	  run();
+	//  Execute the run function.
+	};
 });
